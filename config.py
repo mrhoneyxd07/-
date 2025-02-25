69aeb64c2aacc3b60a2a1d85305ebb1ef38ba432
𓆰𝅃꯭᳚𓄂️𝆺𝅥⃝🔥 ⃪ͥ͢ ᷟ𓆩 𝛭꯭ʀ꯭ 𝗛꯭ᴏ꯭ɴ꯭ᴇ꯭ʏ꯭ 乛|⁪⁬⁮⁮⁮⁮ ‌⁪⁬𓆪🐼™ 𝆺꯭𝅥˶꯭꯭꯭֟፝͟͢͝⏎͟›͢ <Mdhakeeb07302@gmail.com>
2025-02-25 14:20:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NKMODNK6/RessoMusic",
+    "https://github.com/mrhoneyxd07/-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RessoUpdates")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+4GFdUHBOQow4NjM1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/HONEY_STICKERS")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+l0EvAvECd2w4M2Zl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
